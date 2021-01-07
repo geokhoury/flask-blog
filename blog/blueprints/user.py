7e938bb8ec569b1e23b33eabeec796a4f90e52eb
abdallah2124 <abooodmestarihe@gmail.com>
2021-01-07 15:19:46 +0200

    CRUD for user

--- blog/blueprints/user.py
+++ blog/blueprints/user.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Blueprint, render_template, request, redirect,session,flash
+from flask import Blueprint, render_template, request, redirect,session,flash,url_for
 from blog.db import get_db
 from ..forms import EditUserForm,AddUserForm
 
@@ -23,8 +23,6 @@ def add_user():
         last_name = add_user_form.last_name.data
         biography = add_user_form.biography.data
 
-        if add_user_form.validate():
-            print(add_user_form.errors)
 
         # get the DB connection
         db = get_db()
@@ -48,8 +46,8 @@ def add_user():
     # render the template
     return render_template("user/add-user.html",form=add_user_form)
 
-@user_bp.route('/edit/user', methods=['GET', 'POST'])
-def edit_user():
+@user_bp.route('/user/edit/<int:id>', methods=['GET', 'POST'])
+def edit_user(id):
     
     # create instance of our form
     edit_user_form = EditUserForm()
@@ -83,6 +81,12 @@ def edit_user():
             session['last_name'] = last_name
             session['biography'] = biography
 
+            #  flash masseag
+            flash("User information updated successfully!")
+
+            # redirect  
+            return redirect(url_for('user.view_user',id=session['uid']))
+
         except sqlite3.Error as er:
             print('SQLite error: %s' % (' '.join(er.args)))
             return redirect("/404")
@@ -101,5 +105,18 @@ def get_users():
 
     # render 'list.html' blueprint with users
     return render_template('user/list.html', users=users)
+
+@user_bp.route('/user/view/<int:id>')
+def view_user(id):
+    # get the DB connection
+    db = get_db()
+
+    # get user by id
+    user = db.execute(f'''select * from user  WHERE id = {id}''').fetchone()
+
+    # render 'profile.html' blueprint with user
+    return render_template('user/view-user.html', user=user)
+
+
 
 
