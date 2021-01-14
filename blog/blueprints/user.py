97be4e3759cac712084f7d411d373bce0b32ae72
abdallah2124 <abooodmestarihe@gmail.com>
2021-01-14 15:34:09 +0200

    update edit user and create view-user.html

--- blog/blueprints/user.py
+++ blog/blueprints/user.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, session, flash, url_for
 
 from blog.models import User
+# from blog.models import TextUser
 from blog.forms import EditUserForm, AddUserForm
 
 # define our blueprint
@@ -37,15 +38,16 @@ def add_user():
     return render_template("user/add-user.html", form=add_user_form)
 
 
-@user_bp.route('/user/edit/<int:id>', methods=['GET', 'POST'])
+@user_bp.route('/user/edit/<id>', methods=['GET', 'POST'])
 def edit_user(id):
+    user = User.objects(id=id).first()
 
     # create instance of our form
     edit_user_form = EditUserForm()
     if request.method == "GET":
-        edit_user_form.first_name.data = session['first_name']
-        edit_user_form.last_name.data = session['last_name']
-        edit_user_form.biography.data = session['biography']
+        edit_user_form.first_name.data = user.first_name
+        edit_user_form.last_name.data = user.last_name
+        edit_user_form.biography.data = user.biography
 
     # handle form submission
 
@@ -56,30 +58,18 @@ def edit_user(id):
         last_name = edit_user_form.last_name.data
         biography = edit_user_form.biography.data
 
-        print(first_name, last_name)
-        # get the DB connection
-        db = get_db()
+        # Update our user data
 
-        try:
-            # update user information
-            db.execute(
-                f"""UPDATE user SET first_name = '{first_name}', last_name ='{last_name}',biography = '{biography}' WHERE id = '{session['uid']}'   """)
-            db.commit()
+        user.first_name=first_name
+        user.last_name=last_name
+        user.biography=biography
 
-            # update session
-            session['first_name'] = first_name
-            session['last_name'] = last_name
-            session['biography'] = biography
+        # save our changes to the DB
+        user.save()
 
-            #  flash masseag
-            flash("User information updated successfully!")
+         # render the template
+        return redirect(url_for('user.view_user', id=session['uid']))
 
-            # redirect
-            return redirect(url_for('user.view_user', id=session['uid']))
-
-        except sqlite3.Error as er:
-            print('SQLite error: %s' % (' '.join(er.args)))
-            return redirect("/404")
 
     # redner the login template
     return render_template("user/edit-user.html", form=edit_user_form)
@@ -95,13 +85,10 @@ def get_users():
     return render_template('user/list.html', users=users)
 
 
-@user_bp.route('/user/view/<int:id>')
+@user_bp.route('/user/view/<id>')
 def view_user(id):
-    # get the DB connection
-    db = get_db()
 
     # get user by id
-    user = db.execute(f'''select * from user  WHERE id = {id}''').fetchone()
-
+    user = User.objects(id=id).first()
     # render 'profile.html' blueprint with user
     return render_template('user/view-user.html', user=user)
