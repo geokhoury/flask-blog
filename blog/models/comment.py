ce1ee452703c91d14b2f7c0ee3543f3963aa2885
fiddah97 <fiddahm@gmail.com>
2021-01-16 15:07:53 +0200

    add comment

--- blog/models/comment.py
+++ blog/models/comment.py
@@ -1,6 +1,8 @@
 from mongoengine import *
+from .user import User
 
 
 class Comment(EmbeddedDocument):
     content = StringField()
-    name = StringField(max_length=120)
+    # name = StringField(max_length=120)
+    author = ReferenceField(User)
