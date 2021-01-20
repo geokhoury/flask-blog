4ecc147e372fb8e946d66d66a16cec1f36290d92
George Khoury <g@gkhoury.me>
2021-01-20 11:46:58 +0200

    Added change password form. Updated login form.

--- blog/forms/login_forms.py
+++ blog/forms/login_forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField,TextAreaField,SubmitField,PasswordField
 from wtforms.validators import InputRequired,EqualTo
 
 class LoginForm(FlaskForm):
-    username=StringField("Enter your username")
-    password=PasswordField("Enter your password")
+    username=StringField("Username", validators=[InputRequired()], render_kw={"placeholder": "bert"})
+    password=PasswordField("Password", validators=[InputRequired()], render_kw={"placeholder": "secret123"})
     submit=SubmitField("Login")
 
--- blog/forms/user_forms.py
+++ blog/forms/user_forms.py
@@ -1,20 +1,32 @@
-from flask_wtf import FlaskForm 
-from wtforms import StringField,TextAreaField,SubmitField,PasswordField
-from wtforms.validators import InputRequired,EqualTo
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField, PasswordField
+from wtforms.validators import InputRequired, EqualTo, Length
+
+
+class ChangePasswordForm(FlaskForm):
+    current_password = PasswordField(
+        'Enter your current password', [InputRequired()])
+    new_password = PasswordField('Enter your new password', [InputRequired(), EqualTo('confirm_password')])
+    confirm_password = PasswordField(
+        "Confirm your new password", [InputRequired()])
+    change_password = SubmitField("Change password")
 
 
 class EditUserForm(FlaskForm):
     first_name = StringField("Enter your first name")
     last_name = StringField("Enter your last name")
     biography = TextAreaField("Enter your biography")
-    submit=SubmitField("Update User")
+    submit = SubmitField("Update User")
+
 
 class AddUserForm(FlaskForm):
-    username=StringField("Enter your username")
-    password= PasswordField('New Password', [InputRequired(), EqualTo('confirm_password', message='Passwords must match')])
-    confirm_password=PasswordField("Repeat Password")
+    username = StringField("Choose a username", validators=[
+                           InputRequired(), Length(3, 20)])
+    password = PasswordField('Enter a secure password', [InputRequired(), EqualTo(
+        'confirm_password', message='Passwords must match')])
+    confirm_password = PasswordField("Confirm your password")
 
-    first_name = StringField("Enter your first name")
-    last_name = StringField("Enter your last name")
+    first_name = StringField("What is your first name?")
+    last_name = StringField("What is your last name?")
     biography = TextAreaField("Enter your biography")
-    submit=SubmitField("Add User")
+    submit = SubmitField("Create account")
