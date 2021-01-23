9ee33f77acb69625a243c71a3f1a2caa142b72ba
George Khoury <g@gkhoury.me>
2021-01-23 11:37:48 +0200

    Added search form.

--- blog/forms/__init__.py
+++ blog/forms/__init__.py
@@ -6,3 +6,4 @@ print(f'Invoking __init__.py for {__name__}')
 from .login_forms import *
 from .post_forms import *
 from .user_forms import *
+from .search_froms import *
--- blog/forms/post_forms.py
+++ blog/forms/post_forms.py
@@ -4,12 +4,13 @@ from wtforms.validators import InputRequired, EqualTo
 
 
 class AddPostForm(FlaskForm):
-    title = StringField("Enter a title")
-    body = TextAreaField("Enter your text")
-    submit = SubmitField("Add post")
+    title = StringField("Title", render_kw={"placeholder":"Why I love Flask?"})
+    content = TextAreaField("Content", render_kw={"placeholder":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Etiam non quam lacus suspendisse faucibus interdum posuere lorem ipsum. Eu augue ut lectus arcu. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Cursus metus aliquam eleifend mi in nulla. Auctor elit sed vulputate mi sit amet mauris commodo quis. Ac tincidunt vitae semper quis lectus nulla at volutpat. Fames ac turpis egestas integer eget aliquet nibh praesent."})
+    publish = SubmitField("Publish Post")
+    save_draft = SubmitField("Save as Draft")
 
 
 class EditPostForm(FlaskForm):
-    title = StringField("Enter a title")
-    content = TextAreaField("Enter your content")
-    submit = SubmitField("Edit post")
+    title = StringField("Title")
+    content = TextAreaField("Content")
+    update = SubmitField("Update post")
